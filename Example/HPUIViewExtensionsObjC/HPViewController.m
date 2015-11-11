f71064002a298e41672f4014d74161687bacd360
Huy Pham <huyphamthanh8290@gmail.com>
2015-11-11 15:35:20 +0700

    TextField: add placeholer color

--- Example/HPUIViewExtensionsObjC/HPViewController.m
+++ Example/HPUIViewExtensionsObjC/HPViewController.m
@@ -8,22 +8,10 @@
 
 #import "HPViewController.h"
 
-@interface HPViewController ()
-
-@end
-
 @implementation HPViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
