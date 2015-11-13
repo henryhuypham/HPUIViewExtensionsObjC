c3e18868a84ea345cdfe40d671bc774e115c9a65
Huy Pham <huyphamthanh8290@gmail.com>
2015-11-13 17:27:33 +0700

    Reorganize files

---
+++ Pod/Classes/HPUIViewExtensionsObjC.h
@@ -0,0 +1,18 @@
+//
+//  HPUIViewExtensionsObjC.h
+//  Pods
+//
+//  Created by Huy Pham on 11/13/15.
+//
+//
+
+#ifndef HPUIViewExtensionsObjC_h
+#define HPUIViewExtensionsObjC_h
+
+#import "HPView.h"
+#import "HPLabel.h"
+#import "HPTextField.h"
+#import "HPImageView.h"
+#import "HPButton.h"
+
+#endif /* HPUIViewExtensionsObjC_h */
